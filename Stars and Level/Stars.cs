c53e665b3d38d06a793e2411926568176f8c8019
Thorniir <adrian-karski@o2.pl>
2016-10-19 17:22:55 +0200

    Fast_Software
    Waiting for a bus

---
+++ Stars and Level/Stars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stars_Level
+{
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Wpisz ilosc n: ");
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
